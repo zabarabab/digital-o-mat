2b52618f092eb59320086c1bf36546223b2def39
Franzsales2022 <114400548+Franzsales2022@users.noreply.github.com>
2023-01-10 15:23:05 +0100

    Update data.js

--- assets/data/data.js
+++ assets/data/data.js
@@ -1,7 +1,7 @@
 window.omatdata = {
 	"questions": [
 		["Selbstbestimmung","Lehrkräfte des Landes NRW sollten dazu angehalten und dabei unterstützt werden, selbst erstellte Lehrmaterialien freizugeben, sodass jede/r sie nutzen, bearbeiten und weitergeben darf. Es sollte also nicht jeder Lehrkraft selbst überlassen sein, über eine solche Freigabe zu entscheiden."],
-		["Staatliches Hacking","Die Ermittlungsbehörden sollten grundsätzlich nicht, auch nicht mit Durchsuchungsbeschluss, mittels Schadsoftware, Sicherheitslücken oder ähnlicher Verfahren heimlich auf digitale Endgeräte Einzelner zugreifen dürfen."],
+		["Teilhabe & Empowerment","Die Ermittlungsbehörden sollten grundsätzlich nicht, auch nicht mit Durchsuchungsbeschluss, mittels Schadsoftware, Sicherheitslücken oder ähnlicher Verfahren heimlich auf digitale Endgeräte Einzelner zugreifen dürfen."],
 		["Vorhersagende Polizeiarbeit","Die Ermittlungsbehörden sollten auch anlasslos auf Informationen aus sozialen Netzwerken zugreifen und Voraussagen über das Gefahrenpotenzial von Einzelpersonen oder Gruppen, wie etwa Geflüchtete, treffen dürfen."],
 		["Software","Das Land NRW sollte verstärkt Computerprogramme einsetzen und fördern, die für die Allgemeinheit zur kostenfreien Nutzung und Bearbeitung freigegeben sind (Freie und Open-Source-Software)."],
 		["Internetzugang","Kommunen in NRW sollten darauf hinwirken, dass im öffentlichen Stadtraum offene WLAN-Netze zur Verfügung stehen, die keinen kommerziellen Interessen unterliegen."],
